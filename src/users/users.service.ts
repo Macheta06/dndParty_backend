e6d94101a696373e4c3788c25c152d7d20ec5883
Andrés Machetá <sr.macheta08@gmail.com>
2026-07-21 12:44:45 -0500

    Add basic user CRUD

---
+++ src/users/users.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async getUserById(id: number): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async creatUser(data: User): Promise<User> {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+
+  async updateUser(id: number, data: User): Promise<User> {
+    return this.prisma.user.update({
+      where: {
+        id,
+      },
+      data,
+    });
+  }
+
+  async deleteUser(id: number): Promise<User> {
+    return this.prisma.user.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
