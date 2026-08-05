49949ca5cd9edbe7c383ab758cc49e04d38a2c99
Andrés Machetá <sr.macheta08@gmail.com>
2026-08-05 16:42:42 -0500

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { PrismaClientExceptionFilter } from './filters/PrismaClientExceptionFilt
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    credentials: true,
+  });
   app.useGlobalFilters(new PrismaClientExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
